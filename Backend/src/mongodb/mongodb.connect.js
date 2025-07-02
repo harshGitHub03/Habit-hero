20f410fc417b804a643f77c2b37eacb1058df178
harshGit <harshcode07@gmail.com>
2025-07-02 12:54:56 +0530

    updated configs

--- Backend/src/mongodb/mongodb.connect.js
+++ Backend/src/mongodb/mongodb.connect.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
 
-mongoose.connect(`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`)
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Connection successfull to mongodb."))
 .catch((e)=>console.log("Error connecting mongodb.",e))
